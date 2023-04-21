ce4dabbee9e090a989894fd1e569ce1e2c3edccf
Mikhail Radchenko <mradchenko@purefi.io>
2023-04-21 14:54:01 +0300

    chore: rename variables

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,22 +1,22 @@
 import { polygonMumbai, zkSyncTestnet } from 'wagmi/chains';
 
-const STAGE_DASHBOARD_URL = process.env.REACT_APP_STAGE_DASHBOARD_URL;
-const STAGE_ISSUER_URL = process.env.REACT_APP_STAGE_ISSUER_URL;
+const DASHBOARD_URL = process.env.REACT_APP_STAGE_DASHBOARD_URL;
+const ISSUER_URL = process.env.REACT_APP_STAGE_ISSUER_URL;
 
 const CONFIGURED_URLS = {
   [polygonMumbai.id]: {
-    dashboard: STAGE_DASHBOARD_URL,
-    issuer: STAGE_ISSUER_URL,
+    dashboard: DASHBOARD_URL,
+    issuer: ISSUER_URL,
   },
   [zkSyncTestnet.id]: {
-    dashboard: STAGE_DASHBOARD_URL,
-    issuer: STAGE_ISSUER_URL,
+    dashboard: DASHBOARD_URL,
+    issuer: ISSUER_URL,
   },
 };
 
 const DEFAULT_URLS = {
-  dashboard: STAGE_DASHBOARD_URL,
-  issuer: STAGE_ISSUER_URL,
+  dashboard: DASHBOARD_URL,
+  issuer: ISSUER_URL,
 };
 
 export { CONFIGURED_URLS, DEFAULT_URLS };
